068c03e375159f332275f56ee46b6e01b2f1d67e
jongwan <jongbell4@gmail.com>
2025-05-30 11:30:42 +0900

    feat: step01 계산기 / Unit Test

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,14 +1,65 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 public class Calculator {
 
 
-    private Queue<String> values;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        System.out.println("계산하실 연산을 입력해주세요.");
+        String string = scanner.nextLine();
+
+        String[] split = calculator.split(string);
+
+        int calculateResult = calculator.calculate(split);
+
+        System.out.println("연산 결과: "+calculateResult);
+    }
 
     public String[] split(String input){
         return input.split(" ");
+    }
+
+    public int calculate(String[] input){
+        int result = 0;
+        Queue<String> inputQueue = new LinkedList<>(Arrays.asList(input));
+
+        while (!inputQueue.isEmpty()){
+            switch (inputQueue.peek()){
+                case "+":
+                    inputQueue.remove();
+                    result += Integer.parseInt(inputQueue.remove());
+                    break;
+                case "-":
+                    inputQueue.remove();
+                    result -= Integer.parseInt(inputQueue.remove());
+                    break;
+                case "*":
+                    inputQueue.remove();
+                    result *= Integer.parseInt(inputQueue.remove());
+                    break;
+                case "/":
+                    inputQueue.remove();
+                    result /= Integer.parseInt(inputQueue.remove());
+                    break;
+                default:
+                    result = Integer.parseInt(inputQueue.remove());
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isOperator(String peek) {
+        return  peek.equals("+") ||
+                peek.equals("-") ||
+                peek.equals("*") ||
+                peek.equals("/");
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +10,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
 
     @Test
     @DisplayName("공백을 기준으로 문자열을 분리한다.")
     void split_Test(){
         //given
-        Calculator calculator = new Calculator();
         String parameter = "2 + 3 * 4 / 2";
 
         //when
@@ -23,5 +30,19 @@ class CalculatorTest {
         //then
         assertThat(result).containsExactly(
                 "2", "+", "3", "*", "4", "/", "2");
+    }
+
+    @Test
+    @DisplayName("문자열 배열을 전달받아 계산을 수행한 결과를 반환한다.")
+    void calculate_Test(){
+        //given
+        String parameter = "2 + 3 * 4 / 2";
+        String[] splitResult = calculator.split(parameter);
+
+        //when
+        int calculatedResult = calculator.calculate(splitResult);
+
+        //then
+        assertThat(calculatedResult).isEqualTo(10);
     }
 }
